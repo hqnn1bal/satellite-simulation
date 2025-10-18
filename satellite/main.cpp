badd8edc3ec6313416f4f025ce08bf3c960752be
Kirill Evseev <kaevseev@edu.hse.ru>
2025-10-18 15:37:01 +0300

    Added satellite

--- satellite/main.cpp
+++ satellite/main.cpp
@@ -7,7 +7,7 @@ int main() {
         "2 25544  51.6461  21.4681 0001764  87.4306  41.4052 15.49369324256464"
     );
 
-    auto points = sim.simulate(0, 600, 60); // 10 мин по 1 минуте
+    auto points = sim.simulate(0, 600, 60);
     for (auto& p : points) {
         std::cout << p.t_rel_s << "s: lat=" << p.lat_deg << ", lon=" << p.long_deg
             << ", alt=" << p.height_km << "km\n";
